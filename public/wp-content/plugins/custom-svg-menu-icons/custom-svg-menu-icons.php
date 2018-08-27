52920f7d4579c920d4c7d243694ba918b82a72cd
Daniel Gading <daniel.gading@agencychief.com>
2018-08-27 17:37:59 -0400

    Initial icon menu plugin commit

---
+++ public/wp-content/plugins/custom-svg-menu-icons/custom-svg-menu-icons.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @package Custom SVG Menu Icons
+ * @version 1.0
+ */
+/*
+Plugin Name: Custom SVG Menu Icons
+Plugin URI: http://danielgading.com
+Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
+Author: Daniel Gading
+Version: 1.0
+Author URI: http://danielgading.com
+Contributors: dgading
+Text Domain: dg_csmi
+Domain Path: languages
+*/
+//https://gist.github.com/kucrut/3804376
+//https://github.com/ineagu/wp-menu-item-custom-fields
+//http://jafty.com/blog/how-to-add-custom-fields-to-wordpress-navigation-menu-items/
+include 'custom-svg-menu-icons-fields.php';
+
+?>
