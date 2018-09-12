52c26b6bd6e3ae94882c7039a78ff287047961ab
Daniel Gading <daniel.gading@agencychief.com>
2018-09-12 17:43:41 -0400

    Updates typography and hover states

--- public/wp-content/themes/nightrun/template-parts/header.php
+++ public/wp-content/themes/nightrun/template-parts/header.php
@@ -10,27 +10,27 @@
 ?>
 
 <header id="masthead" class="site-header">
-		<div class="site-branding">
+		<div class="site-header__branding">
 			<?php
 			the_custom_logo();
 			if ( is_front_page() && is_home() ) :
 				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<h1 class="site-header__title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 				<?php
 			else :
 				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<p class="site-header__title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
 				<?php
 			endif;
 			$nightrun_description = get_bloginfo( 'description', 'display' );
 			if ( $nightrun_description || is_customize_preview() ) :
 				?>
-				<p class="site-description"><?php echo $nightrun_description; /* WPCS: xss ok. */ ?></p>
+				<p class="site-header__description"><?php echo $nightrun_description; /* WPCS: xss ok. */ ?></p>
 			<?php endif; ?>
-			<svg class="title-decoration back" height="250" width="450">
+			<svg class="site-header__decoration site-header__decoration--back" height="250" width="450">
   			<polygon points="15,45 75,200 380,110" style="fill:rgba(0,0,0,0);stroke:#FF6C11;stroke-width:10;" />
 			</svg>
-			<svg class="title-decoration front" height="250" width="450">
+			<svg class="site-header__decoration site-header__decoration--front" height="250" width="450">
   			<polygon points="105,25 115,210 350,80" style="fill:rgba(0,0,0,0);stroke:#609AAF;stroke-width:10" />
 			</svg>
 		</div><!-- .site-branding -->
